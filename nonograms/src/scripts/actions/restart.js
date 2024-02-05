fc58002ec0fba21d30af706befba326be7e69f02
C0lgate <k0lesan2k00@gmail.com>
2024-02-05 22:07:44 +0300

    refactor: move to new directory

---
+++ nonograms/src/scripts/actions/restart.js
@@ -0,0 +1,14 @@
+export default function () {
+  const gameField = document.querySelector('.game-field');
+  const minutes = document.querySelector('.timer__minutes');
+  const seconds = document.querySelector('.timer__seconds');
+
+  for (const subgrid of gameField.children) {
+    for (const cell of subgrid.children) {
+      cell.setAttribute('data-status', 0);
+    }
+  }
+
+  minutes.textContent = '00';
+  seconds.textContent = '00';
+}

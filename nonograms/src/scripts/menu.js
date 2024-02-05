f86d590b3c1890b02a7049ec59ba4eab8552baea
C0lgate <k0lesan2k00@gmail.com>
2024-02-06 02:12:21 +0300

    fix: add event object in handler

--- nonograms/src/scripts/menu.js
+++ nonograms/src/scripts/menu.js
@@ -2,7 +2,7 @@ import actions from './actions';
 
 const menu = document.querySelector('.menu');
 
-function main() {
+function main(event) {
   const button = event.target.closest('.menu__item');
   const action = actions[button?.getAttribute('data-action')];
 

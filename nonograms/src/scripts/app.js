5a7f4c65e5afa12fde2ebf1e5251990eb42d640e
C0lgate <k0lesan2k00@gmail.com>
2024-02-05 21:34:40 +0300

    feat: add random pattern generation at startup

---
+++ nonograms/src/scripts/app.js
@@ -0,0 +1,16 @@
+import generatorTemplates from './template-generator';
+import sizes from './data/templates';
+
+function main() {
+  const defaultSize = 15;
+  const templates = sizes[`${defaultSize}x${defaultSize}`];
+  const { name, template } =
+    templates[Math.floor(Math.random() * templates.length)];
+
+  generatorTemplates(defaultSize, template);
+}
+
+window.addEventListener('load', main, {
+  passive: true,
+  once: true
+});

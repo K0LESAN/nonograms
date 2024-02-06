fe720aba0ec33a50567e316bd2a9e853f25774d2
C0lgate <k0lesan2k00@gmail.com>
2024-02-06 03:44:19 +0300

    refactor: add empty function and link

--- nonograms/src/scripts/actions.js
+++ nonograms/src/scripts/actions.js
@@ -1,11 +1,9 @@
 import random from './actions/random-game';
 import restart from './actions/restart-game';
 import save from './actions/save-game';
-import continueGame from './actions/continue-game';
 
 export default {
   random,
   restart,
-  save,
-  continue: continueGame
+  save
 };

b2f75b5f7eae71c2ae8593d3601284754072583e
C0lgate <k0lesan2k00@gmail.com>
2024-02-07 00:25:38 +0300

    refactor: remove unused code

--- nonograms/src/scripts/actions/save-game.js
+++ nonograms/src/scripts/actions/save-game.js
@@ -1,25 +1,14 @@
-import transformTemplate from '../helpers/transform-template';
 import { controllerTime } from '../helpers/timer';
 
 export default function () {
   const gameField = document.querySelector('.game-field');
   const length = gameField.children.length;
-  const array = Array.from(gameField.children);
-  const step = Math.sqrt(length);
-  const template = [];
   const savedGame = {
     size: Math.sqrt(length) * 5,
     name: gameField.getAttribute('data-current-template')
   };
 
-  let index = 0;
-
-  while (index < length) {
-    transformTemplate(template, array.slice(index, index + step));
-    index += step;
-  }
-  console.log(gameField);
-  savedGame.savedTemplate = document.querySelector('.game-field').innerHTML;
+  savedGame.savedTemplate = gameField.innerHTML;
   savedGame.template = JSON.parse(
     localStorage.getItem('k32d04sXgxnd312bd-currentGame')
   );

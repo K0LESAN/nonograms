a2043967a634910414b39556c8aa88756dd8d5a1
C0lgate <k0lesan2k00@gmail.com>
2024-02-06 04:19:42 +0300

    feat: add restart time after change random game

--- nonograms/src/scripts/actions/random-game.js
+++ nonograms/src/scripts/actions/random-game.js
@@ -1,5 +1,6 @@
 import templates from '../data/templates';
 import generatorTemplates from '../template-generator';
+import { controllerTime } from '../timer';
 
 export default function () {
   const randomIndex = (arr) => Math.floor(Math.random() * arr.length);
@@ -9,6 +10,9 @@ export default function () {
   const randomTemplate =
     templates[randomSize][randomIndex(randomSize)].template;
   const size = Number(randomSize.split('x')[0]);
+
+  controllerTime.stop();
+  controllerTime.start();
 
   generatorTemplates(size, randomTemplate, templateName);
 }

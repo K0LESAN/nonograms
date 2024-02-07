42467508a5abd6d7a1eb709b04f03adff0ac00a8
C0lgate <k0lesan2k00@gmail.com>
2024-02-07 03:39:28 +0300

    fix: disable saving the game after the solution button

--- nonograms/src/scripts/handlers/menu.js
+++ nonograms/src/scripts/handlers/menu.js
@@ -1,12 +1,17 @@
 import actions from '../actions/actions';
 
+let saveIsNotAllowed = false;
+
 export default function (event) {
   const button = event.target.closest('.menu__item');
-  const action = actions[button?.getAttribute('data-action')];
+  const command = button?.getAttribute('data-action');
+  const action = actions[command];
 
-  if (!button || !action) {
+  if (!button || !action || (command === 'save' && saveIsNotAllowed)) {
     return;
   }
+
+  saveIsNotAllowed = command === 'solution';
 
   action();
 }

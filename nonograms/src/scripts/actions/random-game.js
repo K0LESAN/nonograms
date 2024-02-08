4f5169947c9692b50255847b8aea6ea0550ebc14
C0lgate <k0lesan2k00@gmail.com>
2024-02-08 22:19:29 +0300

    fix: correct calculate caption game field

--- nonograms/src/scripts/actions/random-game.js
+++ nonograms/src/scripts/actions/random-game.js
@@ -5,10 +5,9 @@ export default function () {
   const randomIndex = (arr) => Math.floor(Math.random() * arr.length);
   const sizes = Object.keys(templates);
   const randomSize = sizes[randomIndex(sizes)];
-  const templateName = templates[randomSize][randomIndex(randomSize)].name;
-  const randomTemplate =
-    templates[randomSize][randomIndex(randomSize)].template;
+  const arrTemplates = templates[randomSize];
+  const { name, template } = arrTemplates[randomIndex(arrTemplates)];
   const size = Number(randomSize.split('x')[0]);
 
-  generatorTemplates(size, randomTemplate, templateName);
+  generatorTemplates(size, template, name);
 }
